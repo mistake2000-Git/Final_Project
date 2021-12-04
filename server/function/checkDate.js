c067159d235546e3c3c71a113003caa0c9892c5a
mistake2000-Git <18521002@gm.uit.edu.vn>
2021-12-04 15:36:38 +0700

    fixed

---
+++ server/function/checkDate.js
@@ -0,0 +1,46 @@
+const trans = require('../model/transaction')
+
+async function checkDate(startDate,endDate,roomNum,transId)
+{
+    if(!transId)
+    {
+        startDate = new Date(startDate)
+        endDate = new Date(endDate)
+        let checkRoomInTwoDate = await trans.find({$and:[{Status:{$ne:"Checked-out"}},{$and:[{Room_Num:roomNum},
+            {$or:[{$and:[{Start_Date:{$lte:startDate.getTime()}},{End_Date:{$gte:startDate.getTime()}}]},
+            {$and:[{Start_Date:{$lte:endDate.getTime()}},{End_Date:{$gte:endDate.getTime()}}]}]}]}]})
+
+        let checkRoomOverTwoDate = await trans.find({$and:[{Status:{$ne:"Checked-out"}},{$and:[{Room_Num:roomNum},
+            {$and:[{Start_Date:{$gt:startDate.getTime()}},{End_Date:{$lt:endDate.getTime()}}]}]}]})
+        
+        if(checkRoomInTwoDate.length==0 && checkRoomOverTwoDate.length==0)
+        {
+            return true
+        }
+        else 
+        {
+            return false
+        }
+    }
+    else if(transId)
+    {
+        startDate = new Date(startDate)
+        endDate = new Date(endDate) 
+        let checkRoomInTwoDate = await trans.find({$and:[{id:{$ne:transId}},{$and:[{Status:{$ne:"Checked-out"}},{$and:[{Room_Num:roomNum},
+            {$or:[{$and:[{Start_Date:{$lte:startDate.getTime()}},{End_Date:{$gte:startDate.getTime()}}]},
+            {$and:[{Start_Date:{$lte:endDate.getTime()}},{End_Date:{$gte:endDate.getTime()}}]}]}]}]}]})
+
+        let checkRoomOverTwoDate = await trans.find({$and:[{id:{$ne:transId}},{$and:[{Status:{$ne:"Checked-out"}},{$and:[{Room_Num:roomNum},
+            {$and:[{Start_Date:{$gt:startDate.getTime()}},{End_Date:{$lt:endDate.getTime()}}]}]}]}]})
+
+        if (checkRoomInTwoDate.length==0 && checkRoomOverTwoDate.length==0)
+        {
+            return true
+        }
+        else 
+        {
+            return false
+        }
+    }
+}
+module.exports = checkDate
---
+++ server/model/transaction.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+require('mongoose-moment')(mongoose);
+const transSchema = new Schema({
+    id:{
+        type: String,
+        unique: true,
+        required:true
+    },
+    Customer_Name:{
+        type:String,
+        required:true
+    },
+    Customer_Id_Card:{
+        type:Number,
+        unique:true,
+        require:true
+    },
+    Phone_Number:{
+        type:String,
+    },
+    Room_Num:{
+        type:String,
+        required:true
+    },
+    Start_Date:{
+        type:Date,
+        default: Date.now()
+    },
+    End_Date:{
+        type:Date,
+        required:true
+    },
+    Create_Date:{
+        type:Date, 
+        default: Date.now()  
+    },
+    Payment_Id:
+    {
+        type:String,
+        default:null
+    },
+    Payment_Method:{
+        type:String,
+        default:"Cash",
+        require:true
+    },
+    Status:{
+        type:String,
+        default:"Uncheck-in"
+    },
+    Status_Payment:{
+        type:String,
+        default:"Unpaid",
+        required:true
+    },
+    Total:{
+        type:Number,
+        default:0,
+        required:true
+    }
+})
+module.exports = mongoose.model('trans',transSchema)
